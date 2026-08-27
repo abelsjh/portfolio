6e037f724e2dc6e8385762b9eae47ac148c38ec7
abelsjh_ <abeljason11@gmail.com>
2026-08-27 17:06:42 +0700

    style: rename Navbar button text from Porto to Portfolio for desktop and mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -145,7 +145,7 @@ export default function Navbar() {
               <svg className="w-3.5 h-3.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2.5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
               </svg>
-              <span>Porto</span>
+              <span>Portfolio</span>
             </a>
 
             {/* View Resume Button */}
@@ -272,7 +272,7 @@ export default function Navbar() {
                 <svg className="w-3.5 h-3.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2.5">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 01-2-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                 </svg>
-                <span>Porto</span>
+                <span>Portfolio</span>
               </a>
 
               <a
